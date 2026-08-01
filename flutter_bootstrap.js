0d558e367a36b76d8e67e8eebe64e83963614fea
ThePerfusionist <ThePerfusionist@users.noreply.github.com>
2026-08-01 10:24:21 +0000

    Deploy: version 0.4.0 add swipe/tap to dismiss cardioplegia alert 11e04e1efb33c26a7b36843278684f4adcbd8d90

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"0cd610717bde95fd88343c64f81c11ba4e5c00
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "16585650" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "3965377000" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
